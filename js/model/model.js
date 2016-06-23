bcf67b9e72950fdc0a0641c4764bd76aa6be2445
Lorenzo Hernandez <lorenzo.hernandezjr@gmail.com>
2016-06-23 01:28:17 -0600

    chore: Update location information

--- js/model/model.js
+++ js/model/model.js
@@ -11,32 +11,32 @@ var places = [{
     }
 }, {
     title: 'Mills Building',
-    description: 'Once home to the stables for Ponce de Leon\'s ranch, today this is El Paso\'s town square. The San Jacinto Plaza is also known as the "Plaza of the Lagartos", or Alligator Plaza, because it once had three live alligators in the pond located within.',
-    category: ['Early Years', 'Old-West'],
+    description: 'The Mills Building stands on the original site of the 1832 Ponce de L&eacuteon ranch house. Anson Mills hired Henry C. Trost to design and construct the building. Henry C. Trost came to El Paso in 1903 and was the primary designer for the architectual firm of Trost and Trost. The firm designed hundreds of commercial and residentual buildings including, UTEP, El Paso High School, and Loretto Academy to name a few.',
+    category: ['Early 20th Century'],
     location: {
         lat: 31.758959,
         lng: -106.488860
     }
 }, {
     title: 'Centre Building',
-    description: 'Once home to the stables for Ponce de Leon\'s ranch, today this is El Paso\'s town square. The San Jacinto Plaza is also known as the "Plaza of the Lagartos", or Alligator Plaza, because it once had three live alligators in the pond located within.',
-    category: ['Early Years', 'Old-West'],
+    description: 'The Centre Building, formerly the White House Department Store and Hotel McCoy, is an elegant essay in white terra-cotta in the Chicago Commercial Stryle designed by Henry C. Trost. Opened in 1912, the first floor housed the store and the hotel occupied the other six floors.',
+    category: ['Early 20th Century'],
     location: {
         lat: 31.758637,
         lng: -106.489105
     }
 }, {
     title: 'Plaza Theatre',
-    description: 'Once home to the stables for Ponce de Leon\'s ranch, today this is El Paso\'s town square. The San Jacinto Plaza is also known as the "Plaza of the Lagartos", or Alligator Plaza, because it once had three live alligators in the pond located within.',
-    category: ['Early Years', 'Old-West'],
+    description: 'The Plaza Theatre, designed by W. Scott Dunne in the Spanish Colonial Revival Style, opened on September 12, 1930 to a capacity of 2,410. Built in the midst of the Great Depression, the Plaza Theatre represents the architectual extravagance associated with the glamorous era of the motion-picture industry. Atmospheric effects created the illusion of hte evening sky complete with twinkling stars and floating clouds. Public Tours are conducted every Tuesday at noon.',
+    category: ['Early 20th Century'],
     location: {
         lat: 31.758635,
         lng: -106.489378
     }
 }, {
     title: 'Plaza Hotel',
-    description: 'Once home to the stables for Ponce de Leon\'s ranch, today this is El Paso\'s town square. The San Jacinto Plaza is also known as the "Plaza of the Lagartos", or Alligator Plaza, because it once had three live alligators in the pond located within.',
-    category: ['Early Years', 'Old-West'],
+    description: 'The hotel was constructed on the site of the Hotel Sheldon, which burned in 1929. The Sheldon served as the unofficial headquarters for many of the participants in the Mexican Revolution (1910-1920s) from both sides of the border. In the fall of 1929 Conrad Hilton (Hilton Worldwide Hotels) began construction of his first high-rise hotel.',
+    category: ['Early 20th Century'],
     location: {
         lat: 31.759005,
         lng: -106.488560
